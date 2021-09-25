43d83be82d2080b83669c73f4b76338ecc35186c
Said Mounaim <saidmounaim00@gmail.com>
2021-09-25 21:20:33 +0100

    Add Service Section

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Slider Services
+ */
+$("#services").owlCarousel({
+  stagePadding: 14,
+  item: 3,
+  margin: 20,
+  loop: true,
+  nav: false,
+  dots: true,
+  smartSpeed: 2500,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: false,
+});
