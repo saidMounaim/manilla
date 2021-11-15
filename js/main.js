ef48f823cf503d344cb5094be147c3613c4fa8c0
Said Mounaim <saidmounaim00@gmail.com>
2021-11-15 22:09:27 +0100

    Add Counter Animate On Scroll

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ $("#services").owlCarousel({
   autoplayTimeout: 5000,
   autoplayHoverPause: false,
 });
+
+/**
+ * COUNTER SECTION
+ */
+let nums = document.querySelectorAll(".counter .counter-number");
+let sectionCounter = document.querySelector(".counter");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= sectionCounter.offsetTop - 300) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let number = el.dataset.number;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == number) {
+      clearInterval(count);
+    }
+  }, 2000 / number);
+}
